963cbe78599861178000ac311a9506d1d9b8d081
Vadim <vptrv.pr@gmail.com>
2019-03-30 22:22:10 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ env('APP_URL') }}css/style.css">
+    <link rel="stylesheet" href="{{ env('MIX_URL_SITE') }}css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
@@ -21,13 +21,13 @@
     @yield('content')
 </div>
 
-<script src="{{ env('APP_URL') }}js/vendor/jquery-1.11.2.min.js"></script>
-<script src="{{ env('APP_URL') }}js/vendor/bootstrap.min.js"></script>
-<script src="{{ env('APP_URL') }}js/isotope.pkgd.min.js"></script>
-<script src="{{ env('APP_URL') }}js/owl.carousel.min.js"></script>
-<script src="{{ env('APP_URL') }}js/wow.min.js"></script>
-<script src="{{ env('APP_URL') }}js/custom.js"></script>
-<script src="{{ env('APP_URL') }}js/app.js"></script><!-- /#wrapper -->
+<script src="{{ env('MIX_URL_SITE') }}js/vendor/jquery-1.11.2.min.js"></script>
+<script src="{{ env('MIX_URL_SITE') }}js/vendor/bootstrap.min.js"></script>
+<script src="{{ env('MIX_URL_SITE') }}js/isotope.pkgd.min.js"></script>
+<script src="{{ env('MIX_URL_SITE') }}js/owl.carousel.min.js"></script>
+<script src="{{ env('MIX_URL_SITE') }}js/wow.min.js"></script>
+<script src="{{ env('MIX_URL_SITE') }}js/custom.js"></script>
+<script src="{{ env('MIX_URL_SITE') }}js/app.js"></script><!-- /#wrapper -->
 
 </body>
 
